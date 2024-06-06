158daa3917410e9d022232522d1f4b0888e47620
Aaron Gerald Martillano <aarown.00@gmail.com>
2024-06-07 01:24:59 +0800

    url passing dual approach

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,13 +3,17 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+   #static url route
    path('', views.home, name='home'),
    path('login/', views.login_user, name='login'),
    path('logout/', views.logout_user, name='logout'),
    path('register/', views.register_user, name='register'),
-   path('notices/', views.notices_user, name='notices'),
-   path('createrequest/', views.createrequest_user, name='createrequest'),
-   path('viewrequest/', views.viewrequest_user, name='viewrequest')
-
+   path('unauthorized/', views.unauthorized_user, name='unauthorized'),
+  
+   #dynamic url route
+   path('<str:username>/', views.home, name='user_home'),
+   path('<str:username>/notices/', views.notices_user, name='notices'),
+   path('<str:username>/createrequest/', views.createrequest_user, name='createrequest'),
+   path('<str:username>/viewrequest/', views.viewrequest_user, name='viewrequest')
    
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -4,10 +4,25 @@ from django.contrib import messages
 from .forms import RegisterForm, IT_RequestForm
 from. models import IT_Request
 
+def unauthorized_user(request):
+    return render(request, 'unauthorized.html')
 
-def home(request):
-    it_requests = IT_Request.objects.all()
-    return render(request, 'home.html', {'it_requests': it_requests})
+
+
+
+
+
+
+def home(request, username=None):
+    if request.user.is_authenticated:
+        if request.user.username == username:
+            return render(request, 'home.html', {'username': request.user.username})
+        else:
+            return redirect('unauthorized')
+    else:
+        messages.error(request, "You must be logged in first!")
+        return redirect('login')
+    
 
 def login_user(request):
     if request.method == 'POST':
@@ -17,7 +32,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             messages.success(request, "You have been successfully logged in!")
-            return redirect('home')  
+            return redirect('user_home', username=user.username)
         else:
             messages.error(request, "Accounts credentials invalid! Please try again or register.")
             return render(request, 'login.html')  
@@ -37,12 +52,12 @@ def register_user(request):
 		if form.is_valid():
 			form.save()
 			# Authenticate and login
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, "You Have Successfully Registered! Welcome!")
-			return redirect('home')
+			#username = form.cleaned_data['username']
+			#password = form.cleaned_data['password1']
+			#user = authenticate(username=username, password=password)
+			#login(request, user)
+			messages.success(request, "You Have Successfully Registered! Please log in.")
+			return redirect('login')
 		
 	else:
 		form = RegisterForm()
@@ -51,41 +66,50 @@ def register_user(request):
 	return render(request, 'register.html', {'form':form})
 
 
-def notices_user(request):
+def notices_user(request, username=None):
     if request.user.is_authenticated:
-        it_requests = IT_Request.objects.all()
-        return render(request, 'notices.html', {'it_requests': it_requests})
+        if request.user.username == username:
+            it_requests = IT_Request.objects.all()
+            return render(request, 'notices.html', {'it_requests': it_requests, 'username': request.user.username})
+        else:
+            return redirect('unauthorized')
     else:
         messages.error(request, "You must be logged in first!")
         return redirect('login')
 
 
-def createrequest_user(request):
+def createrequest_user(request, username=None):
     if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = IT_RequestForm(request.POST)
-            if form.is_valid():
-                it_request = form.save(commit=False)
-                it_request.user = request.user  # Set the user field
-                it_request.save()
-                messages.success(request, "Request Submitted!")
-                return redirect('home')
+        if request.user.username == username:
+            if request.method == 'POST':
+                form = IT_RequestForm(request.POST)
+                if form.is_valid():
+                    it_request = form.save(commit=False)
+                    it_request.user = request.user  # Set the user field
+                    it_request.save()
+                    messages.success(request, "Request Submitted!")
+                    return redirect('user_home', username=request.user.username)
+
+            else:
+                form = IT_RequestForm()  # Create an empty form if the request is not POST
+            return render(request, 'createrequest.html', {'form': form})
         else:
-            form = IT_RequestForm()  # Create an empty form if the request is not POST
-        return render(request, 'createrequest.html', {'form': form})
+            return redirect('unauthorized')
     else:
         messages.error(request, "You Must Be Logged In First!")
         return redirect('login')
 	
 
-
-def viewrequest_user(request):
+def viewrequest_user(request, username=None):
     if request.user.is_authenticated:
-        if request.user.is_superuser:
-            it_requests = IT_Request.objects.all()
+        if request.user.username == username:
+            if request.user.is_superuser:
+                it_requests = IT_Request.objects.all()
+            else:
+                it_requests = IT_Request.objects.filter(user=request.user)
+            return render(request, 'viewrequest.html', {'it_requests': it_requests})
         else:
-            it_requests = IT_Request.objects.filter(user=request.user)
-        return render(request, 'viewrequest.html', {'it_requests': it_requests})
+            return redirect('unauthorized')
     else:
         messages.error(request, "You must be logged in first!")
         return redirect('login')
